d14e5d9b7567fe252d5adc9afaa15e44a0e938e4
XDwightsBeetsX <gutierrezjohn2000@gmail.com>
2021-04-25 16:56:45 -0500

    Update Map.py

--- src/Map.py
+++ src/Map.py
@@ -1,89 +1,66 @@
+"""
+Map is primary data struct of topography
+Stores known points, and allows for performing various interpolation schemes on the raw data.  
+"""
 
 
-from .interpolate import *
 from .Point import Point
 
 
 class Map(object):
     """
-    Stores points in matrix form  
+    initialize with list of known points and optionally the size of desired map output  
 
         - Read points from csv  
         - Print points to csv  
         - Output map image
     """
-    def __init__(self, contents=[[]]):
+    def __init__(self, rawData):
         """
-        map is 2D array or vector-like of Points
+        rawData is list of known Points  
+        optional parameters used to determine desired ouput Map size
         """
-        self.Contents = contents
+        self.RawData = rawData
+        
+        # get min/max points of rawData
+        self.xMin, self.xMax = rawData[0].X, rawData[0].X
+        self.yMin, self.yMax = rawData[0].Y, rawData[0].Y
+        for pt in rawData:
+            x, y = pt.X, pt.Y            
+            if x < self.xMin:
+                self.xMin = x
+            elif self.xMax < x:
+                self.xMax = x
+            elif y < self.yMin:
+                self.yMin = y
+            elif self.yMax < y:
+                self.yMax = y
     
     def readFromCsv(self, filename):
         """
         Reads points in from csv in the form:  
             x,y,value
         """
-        # read in csv
         try:
             with open(filename, 'r+') as f:
                 points = []
-                minX, maxX = 0, 0
-                minY, maxY = 0, 0
                 for line in f.readlines():
                     l = line.split(',')
+                    
+                    # TODO read in as double/floats?
                     x = int(l[0])
                     y = int(l[1])
                     val = float(l[2])
-                    if x > maxX: maxX = x
-                    if minX > x: minX = x
-                    if y > maxY: maxY = y
-                    if minY > y: minY = y
+                    
                     p = Point(x, y, val)
                     points.append(p)
+                self.RawData = points
         except Exception:
-            print(f"[MAP]-[readFromCsv]-[ERROR]-could not read points from file '{filename}'")
-        
-        # Set self.Contents
-        for j in range(maxY):   # row by row
-            for p in points:
-                if p.Y == j:    # find points on this row
-                    for i in range(maxX):   # add points to row, or fill with None-Points
-                        if p.X == i:
-                            self.Contents[j][i] = p
-                            print(f"added point: {p.getString()}")
-                        else:
-                            self.Contents[j][i] = Point(i, j, None)
-                
-
-
-
-
+            print(f"[MAP]-[readFromCsv]-[ERROR]-could not read points from file '{filename}'")            
+    
     def addPoint(self, newPt):
-        """
-        Will override if a point has been set at the new point's location
-        """
-        self.Contents[newPt.Y][newPt.X] = newPt
-
-    def getPointsWithData(self):
-        """
-        Get points that have been set before analysis
-        TODO use these for MinMax
-        """
-        pass
-
-    # def getMinMax(self):
-    #     v0 = self.Contents[0][0].Value
-    #     currMax = v0
-    #     currMin = v0
-    #     for i in range(self.xLen):
-    #         for j in range(self.yLen):
-    #             v = self.Contents[i][j].Value
-    #             if currMax < v:
-    #                 currMax = v
-    #             elif v < currMin:
-    #                 currMin = v
-        
-    #     return currMin, currMax
+        self.RawData.append(newPt)
     
     def show(self):
-        print(self.Contents)
+        for pt in self.RawData:
+            print(f"[{pt.X}, {pt.Y}] {pt.Value}")
